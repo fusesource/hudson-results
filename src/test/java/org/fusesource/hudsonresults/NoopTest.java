134c1d102135b80427f0667ddaefee3c68fff13d
Kevin Earls <kevin@kevinearls.com>
2016-08-25 16:11:09 +0200

    Adding test only for Jenkins testing -- remove this when done

---
+++ src/test/java/org/fusesource/hudsonresults/NoopTest.java
@@ -0,0 +1,16 @@
+package org.fusesource.hudsonresults;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Created by kearls on 25/08/16.
+ */
+public class NoopTest {
+    @Test
+    public void makeTheBuildFailTest() {
+        fail("Fix me");
+    }
+
+}
