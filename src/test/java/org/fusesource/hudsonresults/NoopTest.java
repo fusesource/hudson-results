3a15d816bb58140ccfdcb93dd3d0dfb8c61f83c7
Kevin Earls <kevin@kevinearls.com>
2016-08-25 16:12:02 +0200

    Add ignore to failing test

--- src/test/java/org/fusesource/hudsonresults/NoopTest.java
+++ src/test/java/org/fusesource/hudsonresults/NoopTest.java
@@ -1,5 +1,6 @@
 package org.fusesource.hudsonresults;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.fail;
@@ -8,6 +9,7 @@ import static org.junit.Assert.fail;
  * Created by kearls on 25/08/16.
  */
 public class NoopTest {
+    @Ignore
     @Test
     public void makeTheBuildFailTest() {
         fail("Fix me");
