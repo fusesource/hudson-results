fb409641439ee7687a1d864c3f58ed2e3b7ac2a6
Kevin Earls <kevin@kevinearls.com>
2016-09-06 15:21:08 +0200

    Break the build

--- src/test/java/org/fusesource/hudsonresults/NoopTest.java
+++ src/test/java/org/fusesource/hudsonresults/NoopTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.fail;
  * Created by kearls on 25/08/16.
  */
 public class NoopTest {
-    @Ignore
+    @//Ignore
     @Test
     public void makeTheBuildFailTest() {
         fail("Fix me");
